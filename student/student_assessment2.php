dac8171c5df762d14de9affa3eda7fbb0694501d
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-01-12 11:59:27 -0800

    Update student_assessment2.php

--- student/student_assessment2.php
+++ student/student_assessment2.php
@@ -1,5 +1,6 @@
 <?php
-// start the session (access to user: loggedIn, first_name, email, type, course_name, course_id, section_id)
+// start the session 
+// (access to user: loggedIn, first_name, email, type, course_name, course_id, section_id)
 session_start();
 
 // if user is not logged in then redirect them to main page
@@ -17,99 +18,185 @@ if($_SESSION["type"] !== "student"){
 /* GLOBALS */
 $query;
 $res;
-$instr_email;
-$curr_date;
-$past_assessments = array();
-$open_assessments = array();
-$future_assessments = array();
+$pkey;
+$assessment = [];
 
-// connect to the db
-require_once "../register_login/config.php";
+// processing client form data when it is submitted
+if($_SERVER["REQUEST_METHOD"] === "POST"){
 
-// first query - grab instructor email
-$query = "SELECT instructor FROM users WHERE email = '{$_SESSION["email"]}'";
-$res = pg_query($con, $query);
-if(!$res) {
-    // error
-} else {
-    // no error
-    $instr_email = pg_fetch_result($res, 0);
-}
+    // accept $_POST input
+    $pkey = $_POST['pkey'];
 
-// setting to CA timezone
-date_default_timezone_set('America/Los_Angeles');
+    // connect to the db
+    require_once "../register_login/config.php";
 
-$curr_date = date_create();
-$curr_date = date_format($curr_date, "Y-m-d");
-//echo $curr_date, "\n";
-
-// second query - grab all past assessments that belong to user's course_name, course_id, section_id
-$query = "SELECT * FROM assessments WHERE instructor = '{$instr_email}' AND close_date < '{$curr_date}' AND course_name = '{$_SESSION['course_name']}'
-          AND course_id = '{$_SESSION['course_id']}' AND section_id = '{$_SESSION['section_id']}'";
-$res = pg_query($con, $query);
-if(!$res) {
-    // error
-} else {
-    // no error
-    while($row = pg_fetch_row($res)){
-        if(!isset($past_assessments[$row[0]])) {
-            $past_assessments[$row[0]] = [];
-            array_push($past_assessments[$row[0]], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8], $row[9]);
-        }
+    // query - grab the correct assessment
+    $query = "SELECT * FROM assessments WHERE pkey = {$pkey}";
+    $res = pg_query($con, $query);
+    if(!$res) {
+        // error
+    } else {
+        // no error
+        $row = pg_fetch_row($res);
+        array_push($assessment, $row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8], $row[9], $row[10], $row[11], $row[12]);
     }
+    //print_r($assessment);
+
 }
-
-
-// third query - grab all current assessments that belong to user's course_name, course_id, section_id
-$query = "SELECT * FROM assessments WHERE instructor = '{$instr_email}' AND open_date <= '{$curr_date}' AND close_date >= '{$curr_date}'
-          AND course_name = '{$_SESSION['course_name']}' AND course_id = '{$_SESSION['course_id']}' AND section_id = '{$_SESSION['section_id']}'";
-$res = pg_query($con, $query);
-if(!$res) {
-    // error
-} else {
-    // no error
-    while($row = pg_fetch_row($res)){
-        if(!isset($open_assessments[$row[0]])) {
-            $open_assessments[$row[0]] = [];
-            array_push($open_assessments[$row[0]], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8], $row[9]);
-        }
-    }
-}
-
-
-// fourth query - grab all future assessments that belong to user's course_name, course_id, section_id
-$query = "SELECT * FROM assessments WHERE instructor = '{$instr_email}' AND open_date > '{$curr_date}' AND course_name = '{$_SESSION['course_name']}'
-          AND course_id = '{$_SESSION['course_id']}' AND section_id = '{$_SESSION['section_id']}'";
-$res = pg_query($con, $query);
-if(!$res) {
-    // error
-} else {
-    // no error
-    while($row = pg_fetch_row($res)){
-        if(!isset($future_assessments[$row[0]])) {
-            $future_assessments[$row[0]] = [];
-            array_push($future_assessments[$row[0]], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8], $row[9]);
-        }
-    }
-}
-
-/*
-print_r($past_assessments);
-echo "\n";
-print_r($open_assessments);
-echo "\n";
-print_r($future_assessments);
-echo "\n";
-*/
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
-
+        <meta charset="UTF-8">
+        <title><?= $assessment[2]; ?></title>
+        <link rel="stylesheet" href="../assets/css/student/student_assessment2.css" />
+        <link rel="stylesheet" href="../assets/css/global/header.css" />
+        <link rel="stylesheet" href="../assets/css/global/global.css" />
+        <link rel="stylesheet" href="../assets/css/global/footer.css" />
     </head>
     <body>
+        <div id="app">
+            <header>
+                <nav class="container">
+                    <div id="userProfile" class="dropdown">
+                        <button id="userButton" class="dropbtn" onclick="showDropdown()">Hello <?= $_SESSION["first_name"]; ?>!</button>
+                        <div id="myDropdown" class="dropdown-content">
+                            <a href="../register_login/logout.php">Logout</a>
+                        </div>
+                    </div>
 
+                    <div class="site-logo">
+                        <h1 id="OR2STEM-HEADER">
+                            <a id="OR2STEM-HEADER-A" href="student_index.php">On-Ramp to STEM</a>
+                        </h1>
+                    </div>
+
+                    <div class="inner-banner">
+                        <div class="banner-img"></div>
+                    </div>
+                </nav>
+            </header>
+
+            <br>
+
+            <main>
+                <div id="timerDiv">
+                    <h4 class="timer">Timer:</h4>
+                    <h4 class="timer" id="minutes">00</h4> : <h4 class="timer" id="seconds">00</h4>
+                </div>
+
+                <div id="contentDiv">
+                    <p>Hello</p>
+                    <section>
+                        <iframe
+                            style="
+                            overflow: visible;
+                            width: 100%;
+                            height: 900px;
+                            border: 1px solid black;
+                            "
+                            id="frame"
+                            src="https://imathas.libretexts.org/imathas/embedq2.php?id=00000001"
+                            title="LibreTexts"
+                            scrolling="yes"
+                        />
+                    </section>
+                </div>
+            </main>
+
+            <br>
+
+            <footer>
+                <div class="container">
+                    <div class="footer-top flex">
+                        <div class="logo">
+                            <a href="" class="router-link-active"><p>On-Ramp to STEM</p></a>
+                        </div>
+                        <div class="navigation">
+                            <h4>Navigation</h4>
+                            <ul>
+                                <li><a href="../index.html" class="router-link-active">Home</a></li>
+                                <li><a href="" class="">About Us</a></li>
+                                <li><a href="" class="">FAQ</a></li>
+                                <li><a href="" class="">Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div class="navigation">
+                            <h4>External Links</h4>
+                            <ul>
+                                <li><a href="https://scale.fresnostate.edu/scale/" target="_blank"> CSU SCALE </a></li>
+                                <li><a href="http://fresnostate.edu/" target="_blank"> CSU Fresno Homepage </a></li>
+                                <li><a href="http://www.fresnostate.edu/csm/csci/" target="_blank"> Department of Computer Science </a></li>
+                                <li><a href="http://www.fresnostate.edu/csm/math/" target="_blank"> Department of Mathematics </a></li>
+                            </ul>
+                        </div>
+                        <div class="contact">
+                            <h4>Contact Us</h4>
+                            <p> 5241 N. Maple Ave. <br /> Fresno, CA 93740 <br /> Phone: 559-278-4240 <br /></p>
+                        </div>
+                    </div>
+                    <div class="footer-bottom">
+                        <p>© 2021-2022 OR2STEM Team</p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        
+        <script type="text/javascript">
+            /* GLOBALS */
+            const assessment = <?= json_encode($assessment); ?>;
+            console.log(assessment);
+            let timerID; // holds the ID of the timer, used to stop the timer
+
+
+            /* TIMER PORTION */
+            let startTimer = () => {
+                var sec = 0;
+                let pad = (val) => {
+                    return val > 9 ? val : "0" + val;
+                }
+                var timer = setInterval( function() {
+                    document.getElementById("seconds").innerHTML=pad(++sec%60);
+                    document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+                }, 1000);
+                return timer;
+            }
+            // clearTimer stops the timer and resets the clock back to 0
+            let clearTimer = (timerID) => {
+                document.getElementById("seconds").innerHTML= "00";
+                document.getElementById("minutes").innerHTML= "00";
+                clearInterval(timerID);
+            } 
+            // stopTimer just stops the timer
+            let stopTimer = (timerID) => {
+                clearInterval(timerID);
+            }
+
+            // start timer
+            timerID = startTimer();
+
+
+            // controlling the user profile dropdown
+            /* When the user clicks on the button, toggle between hiding and showing the dropdown content */
+            let showDropdown = () => {
+                document.getElementById("myDropdown").classList.toggle("show");
+            }
+            // Close the dropdown if the user clicks outside of it
+            window.onclick = function(event) {
+                if (!event.target.matches('.dropbtn')) {
+                    var dropdowns = document.getElementsByClassName("dropdown-content");
+                    var i;
+                    for (i = 0; i < dropdowns.length; i++) {
+                        var openDropdown = dropdowns[i];
+                        if (openDropdown.classList.contains('show')) {
+                            openDropdown.classList.remove('show');
+                        }
+                    }
+                }
+            }
+
+        </script>
     </body>
 </html>
