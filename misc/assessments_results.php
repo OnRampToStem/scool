2cc30f2a8bf83a65e82c089c12578a85ce4da455
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-01-24 12:19:36 -0800

    Add files via upload

---
+++ misc/assessments_results.php
@@ -0,0 +1,25 @@
+<?php
+// for display purposes
+header('Content-type: text/plain');
+
+echo "Connecting to PostgreSQL database.\n";
+require_once "../register_login/config.php";
+
+$query = "CREATE TABLE IF NOT EXISTS assessments_results (
+    pkey SERIAL PRIMARY KEY,
+    assessment_name TEXT NOT NULL,
+    instructor_email TEXT NOT NULL,
+    student_email TEXT NOT NULL,
+    course_name TEXT NOT NULL,
+    course_id TEXT NOT NULL,
+    date_time_submitted TIMESTAMP NOT NULL,
+    score INT NOT NULL,
+    content JSON NOT NULL
+)";
+pg_query($con, $query) or die("Cannot execute query: {$query}\n" . pg_last_error($con) . "\n");
+echo "The 'assessments_results' table has been successfully created or was already there!\n";
+
+echo "Disconnecting from PostgreSQL database.\n";
+pg_close($con);
+
+?>
