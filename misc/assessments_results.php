f077642aa2c3667dcc7bba068bd5019520d87e86
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-02-01 09:18:56 -0800

    Update assessments_results.php

--- misc/assessments_results.php
+++ misc/assessments_results.php
@@ -10,11 +10,13 @@ $query = "CREATE TABLE IF NOT EXISTS assessments_results (
     assessment_name TEXT NOT NULL,
     instructor_email TEXT NOT NULL,
     student_email TEXT NOT NULL,
+    student_name TEXT NOT NULL,
     course_name TEXT NOT NULL,
     course_id TEXT NOT NULL,
-    date_time_submitted TIMESTAMP NOT NULL,
     score DECIMAL NOT NULL,
-    content JSON NOT NULL
+    max_score DECIMAL NOT NULL,
+    content JSON NOT NULL,
+    date_time_submitted TIMESTAMP NOT NULL
 )";
 pg_query($con, $query) or die("Cannot execute query: {$query}\n" . pg_last_error($con) . "\n");
 echo "The 'assessments_results' table has been successfully created or was already there!\n";
