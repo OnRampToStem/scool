7b43e5c08ee60edc694151c8151a19b5d84189f3
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-02-01 09:20:49 -0800

    Update submit_assessment_results.php

--- student/js-php/submit_assessment_results.php
+++ student/js-php/submit_assessment_results.php
@@ -22,14 +22,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
     $instructor_email = $_POST['instructor_email'];
     $date_time_submitted = $_POST['date_time_submitted'];
     $score = $_POST['score'];
+    $max_score = $_POST['max_score'];
     $content = $_POST['content'];
 
     // connect to the db
     require_once "../../register_login/config.php";
 
     // query to insert results into 'assessments_results' table
-    $query = "INSERT INTO assessments_results (assessment_name, instructor_email, student_email, course_name, course_id, date_time_submitted, score, content)
-              VALUES ('{$assessment_name}', '{$instructor_email}', '{$_SESSION['email']}', '{$_SESSION['course_name']}', '{$_SESSION['course_id']}', '{$date_time_submitted}', {$score}, '{$content}')";
+    $query = "INSERT INTO assessments_results (assessment_name, instructor_email, student_email, student_name, course_name, course_id, score, max_score, content, date_time_submitted)
+              VALUES ('{$assessment_name}', '{$instructor_email}', '{$_SESSION['email']}', '{$_SESSION['name']}', '{$_SESSION['course_name']}', '{$_SESSION['course_id']}', {$score}, {$max_score}, '{$content}', '{$date_time_submitted}')";
     $res = pg_query($con, $query) or die("Cannot execute query: {$query}\n" . pg_last_error($con) . "\n");
 
     echo "Successfully inserted assessment result.";
