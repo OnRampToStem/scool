da6567cf3fcca2fe2558321acd2ec211bcb530b2
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-03-30 12:20:29 -0700

    Add files via upload

--- student/student_assessment1.php
+++ student/student_assessment1.php
@@ -221,9 +221,9 @@ while($row = pg_fetch_row($res)){
                             <h4>Navigation</h4>
                             <ul>
                                 <li><a href="student_index.php">Home</a></li>
-                                <li><a href="../navigation/about-us.php">About Us</a></li>
-                                <li><a href="../navigation/faq.php">FAQ</a></li>
-                                <li><a href="../navigation/contact-us.php">Contact Us</a></li>
+                                <li><a href="../navigation/about-us/about-us.php">About Us</a></li>
+                                <li><a href="../navigation/faq/faq.php">FAQ</a></li>
+                                <li><a href="../navigation/contact-us/contact-us.php">Contact Us</a></li>
                             </ul>
                         </div>
                         <div class="navigation">
