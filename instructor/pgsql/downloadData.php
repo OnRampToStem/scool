6af4c38523c40a7bde0b4b5cbc7c4e796dbaff46
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-09-04 17:13:40 -0700

    Add files via upload

--- instructor/pgsql/downloadData.php
+++ instructor/pgsql/downloadData.php
@@ -1,48 +1,38 @@
 <?php
-/*
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-*/
-// start the session (loggedIn, name, email, type, pic, course_name, course_id, selected_course_name, selected_course_id)
+// start the session //
+// loggedIn, name, email, type, pic, course_name, course_id, selected_course_name, selected_course_id //
 session_start();
 
-// if user is not logged in then redirect them back to Fresno State Canvas
+// if user is not logged in -> redirect them back to Fresno State Canvas //
 if (!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] !== true) {
     header("location: https://fresnostate.instructure.com");
     exit;
 }
 
-// if user account type is not 'Instructor' then force logout
-if ($_SESSION["type"] !== "Instructor") {
+// if user account type is not 'Instructor' or 'Mentor' -> force logout //
+if ($_SESSION["type"] !== "Instructor" && $_SESSION["type"] !== "Mentor") {
     header("location: ../../register_login/logout.php");
     exit;
 }
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // receive data
     $students = json_decode($_POST["students"]);
-
-    $tot_arr = []; // will hold subarrays containing questions answered by the student
+    $data = [];
 
     foreach ($students as $student) {
-        // initialize the file path (student's static questions json file)
-        $filepath = "../../user_data/{$_SESSION['selected_course_name']}-{$_SESSION['selected_course_id']}/questions/{$student['email']}.json";
-        // read the text from the file
+        $filepath = "../../user_data/" . $_SESSION['selected_course_name'] . "-" . $_SESSION['selected_course_id'] . "/questions/" . $student->email . ".json";
         $json_text = file_get_contents($filepath);
-        // convert text to PHP assoc array
         $json_data = json_decode($json_text, true);
 
-        // local array
         $arr = [];
 
-        // loop through each question in the file
+        // loop through each question in the file //
         for ($i = 0; $i < count($json_data); $i++) {
             if ($json_data[$i]["datetime_answered"] !== "") {
-                // create PHP object containing student name & email + question data
+                // create PHP object containing student name & email + question data //
                 $obj = new stdClass();
-                $obj->name = $key;
-                $obj->email = $value;
+                $obj->name = $student->name;
+                $obj->email = $student->email;
                 $obj->tags = $json_data[$i]["tags"];
                 $obj->text = $json_data[$i]["text"];
                 $obj->numCurrentTries = $json_data[$i]["numCurrentTries"];
@@ -64,9 +54,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 array_push($arr, $obj);
             }
         }
-
-        array_push($tot_arr, $arr);
+        array_push($data, $arr);
     }
-
-    echo json_encode($tot_arr);
+    echo json_encode($data);
 }
