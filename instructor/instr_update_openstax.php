50b949429ac906e2df85424963443de19ec5987f
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-11-27 12:16:59 -0800

    Add files via upload

---
+++ instructor/instr_update_openstax.php
@@ -0,0 +1,183 @@
+<?php
+// start the session //
+// (loggedIn, name, email, type, pic, course_name, course_id, selected_course_name, selected_course_id) //
+session_start();
+
+// redirect users if not logged in //
+if (!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] !== true) {
+    header("location: https://fresnostate.instructure.com");
+    exit;
+}
+
+// force logout for non-instructors //
+if ($_SESSION["type"] !== "Instructor") {
+    header("location: ../register_login/logout.php");
+    exit;
+}
+
+// globals //
+$question = null;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // input data //
+    $question_id = $_POST['questionId'];
+
+    // connect to the db //
+    require_once "../register_login/config.php";
+
+    // get question //
+    $query = "SELECT pkey, title, text, pic, numtries, options, rightanswer, isimage, tags, difficulty
+              FROM questions
+              WHERE pkey = $1;";
+    $result = pg_query_params($con, $query, [$question_id]);
+    if ($result) {
+        $question = pg_fetch_assoc($result);
+    } else {
+        die(pg_last_error($con));
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Update OpenStax Question</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/global/global.css" />
+    <link id="css-header" rel="stylesheet" type="text/css" href="" />
+    <link id="css-mode" rel="stylesheet" type="text/css" href="" />
+    <script type="text/javascript">
+        /*
+        const toggleBanner = () => {
+            const cssHeader = document.getElementById("css-header");
+            cssHeader.setAttribute("href", `../assets/css/global/${window.localStorage.getItem("banner")}-header.css`);
+        }
+
+        const toggleCSS = () => {
+            const cssLink = document.getElementById("css-mode");
+            cssLink.setAttribute("href", `../assets/css/instructor/unlock_lo-${window.localStorage.getItem("mode")}-mode.css`);
+        }
+
+        // mode
+        let item = localStorage.getItem("mode");
+        const cssLink = document.getElementById("css-mode");
+        if (item === null) {
+            window.localStorage.setItem('mode', 'OR2STEM');
+            toggleCSS();
+        } else {
+            toggleCSS();
+        }
+
+        // banner
+        item = localStorage.getItem("banner");
+        const cssHeader = document.getElementById("css-header");
+        if (item === null) {
+            window.localStorage.setItem('banner', 'OR2STEM');
+            toggleBanner();
+        } else {
+            toggleBanner();
+        }
+        */
+    </script>
+</head>
+
+<body>
+    <div id="app">
+        <header>
+            <nav class="container">
+                <div id="userProfile" class="dropdown">
+                    <button id="userButton" class="dropbtn" onclick="showDropdown()">Hello <?= $_SESSION["name"]; ?>!</button>
+                    <div id="myDropdown" class="dropdown-content">
+                        <a href="../navigation/settings/settings.php">Settings</a>
+                        <a href="../register_login/logout.php">Logout</a>
+                    </div>
+                    <img id="user-picture" src="<?= $_SESSION['pic']; ?>" alt="user-picture">
+                </div>
+
+                <div class="site-logo">
+                    <h1 id="OR2STEM-HEADER">
+                        <a id="OR2STEM-HEADER-A" href="instr_index1.php">On-Ramp to STEM</a>
+                    </h1>
+                </div>
+
+                <div class="inner-banner">
+                    <div class="banner-img"></div>
+                </div>
+            </nav>
+        </header>
+
+        <div>
+            <form method='POST'>
+                <label for='questionId'>Question ID:</label>
+                <input id='questionId' name='questionId' type='number' required />
+                <input type='submit' />
+            </form>
+        </div>
+
+        <footer>
+            <div class="container">
+                <div class="footer-top flex">
+                    <div class="logo">
+                        <a href="instr_index1.php" class="router-link-active">
+                            <p>On-Ramp to STEM</p>
+                        </a>
+                    </div>
+                    <div class="navigation">
+                        <h4>Navigation</h4>
+                        <ul>
+                            <li><a href="instr_index1.php">Home</a></li>
+                            <li><a href="../navigation/about-us/about-us.php">About Us</a></li>
+                            <li><a href="../navigation/faq/faq.php">FAQ</a></li>
+                            <li><a href="../navigation/contact-us/contact-us.php">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="navigation">
+                        <h4>External Links</h4>
+                        <ul>
+                            <li><a href="instr_index1.php"> CSU SCALE </a></li>
+                            <li><a href="http://fresnostate.edu/" target="_blank"> CSU Fresno Homepage </a></li>
+                            <li><a href="http://www.fresnostate.edu/csm/csci/" target="_blank"> Department of Computer Science </a></li>
+                            <li><a href="http://www.fresnostate.edu/csm/math/" target="_blank"> Department of Mathematics </a></li>
+                        </ul>
+                    </div>
+                    <div class="contact">
+                        <h4>Contact Us</h4>
+                        <p> 5241 N. Maple Ave. <br /> Fresno, CA 93740 <br /> Phone: 559-278-4240 <br /></p>
+                    </div>
+                </div>
+                <div class="footer-bottom">
+                    <p>© 2021-2023 OR2STEM Team</p>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+    <script type="text/javascript">
+        // globals //
+        let question = <?= json_encode($question) ?>;
+
+
+        // controlling the user profile dropdown
+        /* When the user clicks on the button, toggle between hiding and showing the dropdown content */
+        let showDropdown = () => {
+            document.getElementById("myDropdown").classList.toggle("show");
+        }
+        // Close the dropdown if the user clicks outside of it
+        window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
+    </script>
+</body>
+
+</html>
