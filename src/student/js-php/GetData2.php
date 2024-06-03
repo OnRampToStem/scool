002b1c8756937fae006c5417ae565c558af9d950
John Wagenleitner <johnwa@mail.fresnostate.edu>
2024-06-03 08:56:12 -0700

    move php files to `src` directory

---
+++ src/student/js-php/GetData2.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * Student Centered Open Online Learning (SCOOL) LTI Integration
+ * Copyright (c) 2021-2024  Fresno State University, SCOOL Project Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+	$x1=file_get_contents('../../assets/json_data/openStax.json');
+	$x2=file_get_contents('../../assets/json_data/static_questions_id.json');
+	$x3=file_get_contents('../../assets/json_data/static_questions_info.json');
+	$x4=file_get_contents('../../assets/json_data/Kmap.json');
+	
+	$array = ['ChapterList' => $x1,
+			  'StaticQuestionsID' => $x2 ,
+			  'StaticQuestionsInfo' => $x3,
+			  'kMap' => $x4];
+	echo json_encode($array);
+
+?>
